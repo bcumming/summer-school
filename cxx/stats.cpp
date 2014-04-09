2d2f96360f37b725a0f5b8bb4e603db148d62c46
Dmitry Mikushin <dmitry@kernelgen.org>
2014-04-10 00:28:57 +0200

    Adding transitional C++ version, to be used as a base for CUDA version

---
+++ cxx/stats.cpp
@@ -0,0 +1,4 @@
+#include "stats.h"
+
+unsigned long long stats::flops_diff, stats::flops_bc, stats::flops_blas1;
+
---
+++ cxx/stats.h
@@ -0,0 +1,10 @@
+#ifndef STATS_H
+#define STATS_H
+
+namespace stats
+{
+	extern unsigned long long flops_diff, flops_bc, flops_blas1;
+}
+
+#endif // STATS_H
+
