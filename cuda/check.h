942513bd865a8f65ca60d9f0a630cafaaa59e3c3
Dmitry Mikushin <dmitry@kernelgen.org>
2014-07-01 03:56:55 +0400

    Disabling verbose error checking by default

--- cuda/check.h
+++ cuda/check.h
@@ -1,10 +1,13 @@
 #ifndef CHECK_H
 #define CHECK_H
 
+#if 1
+#define CUDA_ERR_CHECK(x) x;
+#define CUDA_LAUNCH_ERR_CHECK(...) __VA_ARGS__;
+#else
 #include <assert.h>
 #include <cstdio>
 #include <cstdlib>
-
 #define CUDA_ERR_CHECK(x)                                   \
     do { cudaError_t err = x;                               \
         if (err != cudaSuccess) {                           \
@@ -20,6 +23,7 @@
         (int)err, cudaGetErrorString(err),                  \
         __FILE__, __LINE__); assert(false);                 \
     }} while (0);
+#endif
 
 #endif // CHECK_H
 
